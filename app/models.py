652a024f45a763f5b1531db0ebecb10036a0824a
Bruno Leonardo <brunoleorosa@gmail.com>
2021-04-09 16:07:51 -0300

    Alteração no atributo servico (inserção de lista com serviços disponíveis)

--- app/models.py
+++ app/models.py
@@ -8,7 +8,14 @@ class Empresa(models.Model):
 
 class Atestados(models.Model):
     numero_documento = models.CharField(max_length=30)
-    tipo_de_servico = models.CharField(max_length=100)
-    # its = models.CharField(max_length=100)
+
+    servico = (('Desenvolvimento', 'Desenvolvimento'),
+               ('ITS', 'ITS'),
+               ('Service Desk', 'Service Desk'),
+               ('Suporte', 'Suporte'),
+               )
+
+    tipo_de_servico = models.CharField(max_length=30, choices=servico, blank=True)
     data_emissao = models.DateField(default=datetime.now)
+#    documento_pdf = models.FileField(upload_to='pdf')
     empresa = models.ForeignKey(Empresa, on_delete=models.RESTRICT, null=False)
